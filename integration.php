2e9d142df482a0c6753505f0019b005f6e4a97c1
Gwen-Pigat <pixofheaven@gmail.com>
2015-02-25 22:13:55 +0100

    Test merge

--- integration.php
+++ integration.php
@@ -2,14 +2,14 @@
 <html>
 <head>
 	<meta charset="utf-8">
-<<<<<<< HEAD:integration.php
+
 	<link rel="stylesheet" type="text/css" media="screen and (min-width: 500px)" href="css/integration.css">
 	<link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="css/mobileHeader.css">
 	<link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="css/mobileIntegration.css">
-=======
+
 	<link rel="stylesheet" type="text/css" href="css/integration.css">
 	<link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="css/mobile.css">
->>>>>>> 3e56f028b503af66c41cfc4701dc75ef6e1a0ab7:integration.php
+
 	<title>Hackathon intégration</title>
 </head>
 <body>
@@ -31,15 +31,9 @@
 		<a href="#"><img class="menu actu" src="img/actu.png"></img></a>
 		<a href="#"><img class="menu question" src="img/question.png"></img></a>
 	</div>
-<<<<<<< HEAD:integration.php
+
 
 <?php include("Footer.php"); ?>
 
-
-=======
-	
-	<?php include("Footer.php"); ?>
-	
->>>>>>> 3e56f028b503af66c41cfc4701dc75ef6e1a0ab7:integration.php
 </body>
 </html>
