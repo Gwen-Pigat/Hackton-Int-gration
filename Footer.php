e66dc291f682654955c01ef71e2bb20891a3ae1e
Setzu <setzu28@gmail.com>
2015-02-25 22:25:10 +0100

    ajout footer php

--- Footer.php
+++ Footer.php
@@ -1,46 +1,35 @@
 <html>
 <head>
   <title>Hackton Intégration</title>
-  <link rel="stylesheet" type="text/css" media="screen and (min-width: 500px)" href="css/Footer.css">
-  <link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="css/mobileIntegration.css">
+  <link rel="stylesheet" type="text/css" media="screen and (min-width: 500px)" href="css/integration.css">
+  <link rel="stylesheet" type="text/css" media="screen and (max-width: 500px)" href="css/mobile.css">
   <meta charset="utf-8">
 </head>
 <body>
 
-<div class="white">
-
-<p>SUIVEZ-NOUS<img src="img/social-icons.png"> </p>
-
-
-<p class="green">CONTACT</p>
-</div>
-
-<div class="main">
-
-<div class="logo">
-<img src="img/logo.png">
-
-</div>
-
-<div class="bloc1">
-<h3>PAYS PERCHE D'EURE ET LOIR</h3>
-<p>18 rue de la gare</p>
-<p>28240 LA LOUPE</p>
-<p>02 37 29 09 29 payperche.slap@wanadoo.fr</p>
-</div>
-<div class="bloc2">
-<h3>JE VEUX RECEVOIR <br>LA LETTRE D'INFORMATION</h3>
-<p class="email">email</p>
-</div>
-
-<img class="mail" src="img/mail.png">
-
-<div class="contact">
-
-<p>contactez-nous</p>
-</div>
-
-</div>
-
+	<div class="white">
+		<p class="folow">SUIVEZ-NOUS </p>
+		<img class="img-social" src="img/social-icons.png">
+		<p class="green">CONTACT</p>
+	</div>
+	<div class="bloc1">
+		<div class="div-logo">
+			<img class="logo-perche" src="img/logo.png">
+		</div>
+		<div class="info">
+			<h3>PAYS PERCHE D'EURE ET LOIR</h3>
+			<p>18 rue de la gare<br>28240 LA LOUPE<br>02 37 29 09 29 payperche.slap@wanadoo.fr</p>
+		</div>
+		<div class="div-lettre">
+			<h3 class="lettre">JE VEUX RECEVOIR <br>LA LETTRE D'INFORMATION</h3>
+			<a href="#"><p class="email">email</p></a>
+		</div>
+		<a href="#"><div class="contact">
+			<div class="div-mail">
+				<img class="mail" src="img/mail.png">
+			</div>
+			<p>contactez-nous</p>
+		</div></a>
+	</div>
 </body>
 </html>
